922c6fd1b262786aafa8e4863f70c32c1d99d7af
dumebai <18.adriandumitru@gmail.com>
2025-07-23 00:27:45 +0300

    Initial commit for Rafala landing

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,12 +10,10 @@ export default function Hero() {
         return () => clearInterval(interval);
     }, []);
     return (
-        <section className="relative h-[calc(100vh-300px)] w-full bg-noir flex items-center justify-center">
-            <div className="absolute inset-0 z-10 flex items-center justify-center">
-                <h3 className="text-glacier text-2xl md:text-3xl font-display leading-tight text-center">
-                    Work in progress{dots}
-                </h3>
-            </div>
+        <section className="relative h-screen w-full bg-noir flex items-center justify-center">
+            <h3 className="text-glacier text-2xl md:text-3xl font-display leading-tight text-center">
+                Work in progress{dots}
+            </h3>
         </section>
     );
 }
